3ffeedebf5aebe958bbba543a7e040cc178ccf56
Jon Borgonia <jon.borgonia@gmail.com>
2024-10-01 10:36:44 -0700

    feat: CyberNvim config

--- README.md
+++ README.md
@@ -9,8 +9,13 @@ link configs (remove originals if needed)
 ln -s ~/.dotfiles/fish ~/.config/fish
 ln -s ~/.dotfiles/tmux.conf  ~/.tmux.conf
 ln -s ~/.dotfiles/tmux-line.conf  ~/.config/tmux-line.conf
-ln -s ~/.dotfiles/lvim/  ~/.config/lvim
 ln -s ~/.dotfiles/kitty/  ~/.config/kitty
+```
+
+Setup CyberNvim and link user config
+```sh
+git clone https://github.com/pgosar/CyberNvim ~/.config/nvim
+ln -s ~/.dotfiles/CyberNvim ~/.config/nvim/lua/user
 ```
 
 Install [fisherman](https://github.com/jorgebucaran/fisher?tab=readme-ov-file)
--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -2,12 +2,12 @@
 if not set -q __fish_path_set
   set -g __fish_path_set 1
 
-  set -g -x EDITOR /home/remix/.local/bin/lvim
+  set -g -x EDITOR /opt/nvim-linux64/bin/nvim
   set -g -x GIT_EDITOR $EDITOR
   set -g -x GOPATH ~/.gopath
 
   # for dap-install jsnode
   set -g -x NODE_OPTIONS --no-experimental-fetch
 
-  set -U fish_user_paths ~/.local/bin $GOPATH/bin ~/.fzf/bin ~/.config/fnm/bin /usr/local/go/bin
+  set -U fish_user_paths ~/.local/bin $GOPATH/bin ~/.fzf/bin ~/.config/fnm/bin /usr/local/go/bin /opt/nvim-linux64/bin
 end
